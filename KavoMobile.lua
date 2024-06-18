cd1f1a508f379159cd80d96ba58371fada0b2e6c
cristian <129870122+aosthedev@users.noreply.github.com>
2024-06-18 12:56:18 +0200

    Update KavoMobile.lua

--- KavoMobile.lua
+++ KavoMobile.lua
@@ -57,7 +57,7 @@ local themes = {
 }
 local themeStyles = {
     DarkTheme = {
-        SchemeColor = Color3.fromRGB(0, 0, 0),
+        SchemeColor = Color3.fromRGB(1, 52, 133),
         Background = Color3.fromRGB(0, 0, 0),
         Header = Color3.fromRGB(0, 0, 0),
         TextColor = Color3.fromRGB(255,255,255),
